67a9b6a69e74b28c851a24ea6437ae0d52a73664
lizziebrannon <lizziebrannon@yahoo.com>
2025-04-25 20:23:33 -0500

    Update outline.py

--- outline.py
+++ outline.py
@@ -1,9 +1,31 @@
-class SalesReport:
+class Customer:
+  customerName = getName()
+  customerAddress = getAddress()
+  customerDateofBirth = getDateofBirth()
+  customerEmail = getEmail()
+  customerPhone = getPhone()
+
+  def __init__(customer):
   
-  @staticmethod
-  def printReport():
+  def getName():
+    name = input("Enter name: ")
+    return name
 
+  def getAddress():
+    address = input("Enter address: ")
+    return address
+    
+  def getDateofBirth():
+    dateofBirth = input("Enter date of birth: ")
+    return dateofBirth
+    
+  def getEmail():
+    email = input("Enter eMail: ")
+    return email
+  
+  def getPhone():
+    customerPhone = int(input("Enter phone number: ")
+    return customerPhone
 
-
-
+class SalesTax:
   

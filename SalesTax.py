028fbbf98553fcb45f8e60333c861688170e73a5
lizziebrannon <lizziebrannon@yahoo.com>
2025-04-26 14:21:02 -0500

    Update SalesTax.py

--- SalesTax.py
+++ SalesTax.py
@@ -2,8 +2,8 @@ import datetime # import datetime module to retrieve date of sale and generate u
 
 class SalesTax: # calculate sales tax
   def __init__(self):
-    self.taxRate = 0.0625
-    self.bookPrice = bookPrice
+    self.taxRate = 0.0625 # set taxRate to 0.625
+    self.bookPrice = 0.0
     self.customerName = ''
     self.salesDate = ''
     self.purchaseID = ''
@@ -12,19 +12,19 @@ class SalesTax: # calculate sales tax
     while True: # validation loop to get price
       try:
         self.bookPrice = float(input("Enter item price: $"))
-        if price >= 0.0:
+        if price >= 0.0: # return price if it is positive
           return bookPrice
         else:
           print("Invalid entry. Please try again")
-      except ValueError:
+      except ValueError: # catch non-float entries
         print("Price must be numeric. Please try again.")
 
-  def calculateTax(self):
+  def calculateTax(self): # calculate sales tax, this does NOT calculate the total price, just the price of the tax itself
     return self.bookPrice * self.taxRate
 
   def makePurchaseID(self):
     self.customerName = input("Enter customers first and last name: ").strip()
     now = datetime.datetime.now()
-    self.salesDate = '%02d%02d%02d%02d%02d%02d' %(now.hour, now.minute, now.second, now.month, now.day, now.year)
-    self.purchaseID = self.salesDate + self.customerName
+    self.salesDate = '%02d%02d%02d%02d%02d%02d' %(now.hour, now.minute, now.second, now.month, now.day, now.year) # generate the sales date
+    self.purchaseID = self.salesDate + self.customerName # combine the sales date and the customers name to make a unique purchase id
     return self.purchaseID

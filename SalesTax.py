e418360902b3584c1e26b13d51e9ae2be3eafd43
lizziebrannon <lizziebrannon@yahoo.com>
2025-04-26 12:59:53 -0500

    Update SalesTax.py

--- SalesTax.py
+++ SalesTax.py
@@ -1,13 +1,19 @@
 class SalesTax: # calculate sales tax
-  basePrice = getBookPrice()
+  def __init__(self, tax):
+    self.tax = salestax
 
-  def getBookPrice():
-    bookPrice = float(input("Enter price: "))
-    return bookPrice
+  def getBookPrice(self): # get the base price of the book
+    while True: # start input validation for the price
+      try:
+        bookPrice = float(input("Enter the base price: $"))
+        if bookPrice > 0: # return variable if it is a positive, non-zero integer.
+          return bookPrice
+        else:
+          print("Invalid entry. Base price must be an integer that is positive and non-zero.")
+      except ValueError: # catch non-integer entries
+          print("Invalid entry. Base price must be an integer.")
 
-  def calculateTax(): #calculate the sales
-    base_price = getBookPrice()
-    tax = .0625
+  def calculateTax(self, bookPrice): #calculate the sales
     calculatedTax = base_price + (base_price * tax)
     return calculatedTax
 

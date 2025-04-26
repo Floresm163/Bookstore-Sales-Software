45843b6d7a10e3b20ffc6df0365f99ae636aa3a6
lizziebrannon <lizziebrannon@yahoo.com>
2025-04-26 12:03:29 -0500

    Update outline.py

--- outline.py
+++ outline.py
@@ -1,33 +1,5 @@
-class Customer: # get customer information
-  customerName = getName()
-  customerAddress = getAddress()
-  customerDateofBirth = getDateofBirth()
-  customerEmail = getEmail()
-  customerPhone = getPhone()
-      
-  def getName(): # get customers name
-    name = input("Enter name: ")
-    return name
-
-  def getAddress(): # get customers address
-    address = input("Enter address: ")
-    return address
-    
-  def getDateofBirth(): # get customers date of birth
-    dateofBirth = input("Enter date of birth: ")
-    return dateofBirth
-    
-  def getEmail(): # get customers email
-    email = input("Enter eMail: ")
-    return email
-  
-  def getPhone(): # get customers phone number
-    customerPhone = int(input("Enter phone number: "))
-    return customerPhone
-
 class SalesTax: # calculate sales tax
   basePrice = getBookPrice()
-  customer
 
   def getBookPrice():
     bookPrice = float(input("Enter price: "))
@@ -41,16 +13,3 @@ class SalesTax: # calculate sales tax
 
   @staticmethod # generate some kind of ID to attach to sale
   def makePurchaseID():
-      
-  
-class SupportTicket: # create and edit support tickets
-  
-  ticketNumber = 
-  ticketDescription = 
-  customerName = 
-  customerEmail = 
-
-  def addSupportTicket():
-
-  
-  

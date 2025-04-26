6ccb4d17c36d35ba780f70d37404152256514f97
lizziebrannon <lizziebrannon@yahoo.com>
2025-04-26 17:10:41 -0500

    Update SupportTicketClass.py

--- SupportTicketClass.py
+++ SupportTicketClass.py
@@ -34,22 +34,68 @@ def getCustomerName(self): # get the customers name
     else:
       print("Invalid entry. Support ticket must contain a valid name.")
 
+def setTicketStatus(self, status='open'):
+  validStatus = ['open', 'closed']
+  if status.lower() in validStatus:
+    return status.lower()
+  return 'open'
+  
+
 def addSupportTicket(self): # retrieve all the values and create the ticket
   ticketNumber = self.getTicketNumber()
   customerName = self.getCustomerName()
   customerEmail = self.getCustomerEmail()
   description = self.getTicketDescription()
+  status = self.setTicketStatus()
 
-  ticket = {'Ticket Number': ticketNumber, 'Customer Name': customerName, 'Customer Email': customerEmail, 'Description': description, 'Status': 'Open'}
+  ticket = {'Ticket Number': ticketNumber, 'Customer Name': customerName, 'Customer Email': customerEmail, 'Description': description, 'Status': status}
   self.tickets[ticketNumber] = ticket
   print(f'\nTicket #:{ticketNumber} created successfully.')
   return ticketNumber
 
-def removeSupportTicket(self): # delete a ticket
+class ManageSupportTicket:
+  def __init__(self, supportTickets):
+    self.Support = supportTickets
 
-def editSupportTicket(self): # edit an already existing ticket
+  def editSupportTicket(self, ticketNumber): # edit an already existing ticket
+    if ticketNumber not in self.Support.tickets: # if ticket not found print error message
+      print(f'Ticket #{ticketNumber} not found.')
+      return False
 
-def updateTicketStatus(self): # update the status of the ticket
+    ticket = self.supportTickets[ticketNumber]
+    print(f'Editing Ticket #:{ticketNumber}')
+    print(f'Enter new value or press enter to skip field.')
+  
+    new_first = input('Enter new first name: ')
+    new_last = input('Enter new last name: ')
+    newName = new_first + new_last
+    if newName: # if name is empty then the field was skipped an will not update
+      ticket['customerName'] = newName
+
+    newEmail = input('Enter new eMail address: ')
+    if newEmail:
+      ticket['customerEmail'] = newEmail
+
+    newDescription = input('Enter new description: ')
+    if newDescription:
+      ticket['description'] = newDescription
+
+    while True:
+      newStatus = input("Set ticket status to 'open' or 'closed'").lower()
+      if newStatus == 'open' or newStatus == 'closed':
+        ticket['status'] = newStatus
+        break
+      else:
+        print('Invalid entry. Ticket status must be set to open or closed.')
+
+    print(f'Ticket #{ticketNumber} has been succesfully updated.')
+    return True
+    
+  def removeSupportTicket(self): # delete a ticket
+    
+  
+  
+  def updateTicketStatus(self): # update the status of the ticket
   
   
   

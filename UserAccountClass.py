18216f18ab28f74bc9765a983c3846dc9681b885
lizziebrannon <lizziebrannon@yahoo.com>
2025-04-27 12:11:44 -0500

    Update UserAccountClass.py

--- UserAccountClass.py
+++ UserAccountClass.py
@@ -1,67 +1,108 @@
-class Customer: # get customer information and store in dictionary
-  def __init__(self): # set all variables to empty by default
-    self.UserAccount = {'Name': 
-    self.customerName = ''
-    self.customerAddress = ''
-    self.customerDateofBirth = ''
-    self.customerEmail = ''
-    self.customerphone = ''
+class UserAccounts: # create user accounts
+  def __init__(self): # create a dictionary to store account information. Set all variables to empty by default except account type which is set to customer
+    self.UserAccount = {'Name': None,
+                        'User Name': None,
+                        'Password': None,
+                        'eMail': None,
+                        'Phone Number': None,
+                        'Date of Birth': None,
+                        'Address': None,
+                        'Account Type': 1}
 
-def getName(self): # get customers name
+def getName(self): # get account owners name
   while True: # start validation loop to make sure name is not empty
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
+    first_name = input("Enter first name: ").upper()
+    last_name = input("Enter last name: ").upper()
     full_name = first_name + last_name
     if name: # checks if the name has been entered, if not repeat the loop
-      self.customerName = full_name
+      self.UserAccount['Name'] = full_name
       break
-    print("Invalid Entry. Please enter a valid name.")
+    else:
+      print("Invalid Entry. Enter a valid name.")
 
-def getAddress(self): # get customers address
+def getUserName(self): # get account users user name for login
   while True: 
-    address = input("Enter address: ")
-    if address: 
-      self.customerAddress = address
+    userName = input("Enter user name: ").upper()
+    if userName: 
+      self.UserAccount['User Name'] = userName
       break
-    print("Invalid Entry. Please enter a valid address.")
-    
-def getDateofBirth(self): # get customers date of birth
-  while True: 
-    try:
-      dob = int(input("Enter date of birth in MMDDYYYY format: ")).strip()
-      if len(dob) == 8: # set variable if entry is approriate length
-        self.customerDateofBirth = dob
-        break
-      else:
-        print("Invalid Entry. Please enter a valid date of birth.")
-    except ValueError: # catch non-integer entries
-      print("Invalid Entry. Please enter a valid date of birth.")
+    else:
+      print("Invalid Entry. Enter a valid user name.")
 
-def getEmail(self): # get customers email
+def getPassword(self): # get account users password
+  while True: 
+    password = input('Enter a password (must be at least 8 characters): ').strip()
+    if len(password) < 8:
+      print("Password must be at least 8 characters long.")
+      continue
+
+    confirm = input('Confirm your password: ').strip() # make users enter password again 
+    if password == confirm:
+      self.UserAccount['Password'] = password
+      break
+    else:
+      print('Invalid entry. Passwords do not match.')
+
+def getEmail(self): # get account users email
   while True: 
     email = input("Enter eMail address: ")
     if '@' in email: # basic email validation
-      self.cutomerEmail = email
+      self.UserAccount['eMail'] = email
       break
-    print("Invalid Entry. Please enter a valid email address.")
-  
-def getPhone(self): # get customers phone number
+    print("Invalid Entry. Enter a valid email address.")
+
+def getPhone(self): # get account users phone number
+  while True: 
+    phone = int(input("Enter phone number: ")).strip()
+    if len(phone) == 10 and phone.isdigit(): # set variable if entry is approriate length and only numbers
+      self.UserAccount['Phone'] = phone
+      break
+    else:
+      print("Invalid Entry. Enter a valid phone number.")
+
+def getDateofBirth(self): # get account users date of birth
+  while True: 
+    dob = int(input("Enter date of birth in MMDDYYYY format: ")).strip()
+    if len(dob) == 8 and dob.isdigit(): # set variable if entry is approriate length
+      self.UserAccount['Date of Birth'] = dob
+      break
+    else:
+      print("Invalid Entry. Enter a valid date of birth.")
+
+def getAddress(self): # get account users address
+  while True: 
+    address = input("Enter address: ")
+    if address: 
+      self.UserAccount['Address'] = address
+      break
+    print("Invalid Entry. Enter a valid address.")
+
+def setAccountType(self): # assign the profiles designation, 1 = administrator, 2 = staff, 3 = customer
   while True: 
     try:
-      phone = int(input("Enter phone number: ")).strip()
-      if len(phone) == 10: # set variable if entry is approriate length
-        self.customerPhone = phone
+      accountType = int(input("Enter Account Designation (1 = Administrator, 2 = Staff, 3 = Customer): "))
+      if 1 <= accountType <= 3:
+        self.UserAccount['Account Type'] = accountType
         break
       else:
-        print("Invalid Entry. Please enter a valid phone number.")
-    except ValueError: # catch non-integer entries
-      print("Invalid Entry. Please enter a valid phone number.")
+        print("Invalid entry. Account designation must be between 1 and 3.")
+        
 
-def addCustomer(self): # this can be removed if we want but just for the sake of convenience probably easier to get info all at once
-  print('\nPlease enter customer information: ')
-  self.getName
-  self.getAddress
-  self.getDateofBirth
-  self.getEmail
-  self.getPhone
+class ManageUserAccounts:
+  def __init__(self):
+    self.users = {}
 
+  def addUserAccount(self):
+    newAccount = UserAccounts()
+    newAccount = UserAccounts()
+    newAccount.getName()
+    newAccount.getUserName()
+    newAccount.getPassword()
+    newAccount.getEmail()
+    newAccount.getPhone()
+    newAccount.getDateofBirth()
+    newAccount.getAddress()
+
+    username = newAccount.
+    
+    

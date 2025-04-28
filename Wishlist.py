fb9ef0e51224d4145eaf5c453b71ae6cb69fc538
ElisaMujica <elisa.m.mujica@gmail.com>
2025-04-28 14:12:40 -0500

    Fix wishlist issues
    Fix wrong constructor type
    Fix indentation in addTOWishList 
    Fix wrong variable from book to Wishlist book 
    Add missing colon 
    Fix variable type on book ID

--- Wishlist.py
+++ Wishlist.py
@@ -1,28 +1,28 @@
 class Wishlist:
-# Creating an empty wishlist
-  def _init_(self):
-    self.books = []
+    # Creating an empty wishlist
+    def __init__(self):
+        self.books = []
 
-# Displaying your wishlist if you have a wishlist
-  def displayWishList(self) -> None:
-    if not self.books:
-      print('Your Wishlist is empty.')
-    else:
-      print('----Your Wishlist-----')
-      for book in self.books:
-        print(f'{book.getBookPublishing()} - ${book.getPrice()}')
+    # Displaying your wishlist if you have a wishlist
+    def displayWishList(self) -> None:
+        if not self.books:
+            print('Your Wishlist is empty.')
+        else:
+            print('----Your Wishlist-----')
+            for book in self.books:
+                print(f'{book.getBookPublishing()} - ${book.getPrice():.2f}')
 
-# Adding a book from the BookListing to the Wishlist
-def addToWishlist(self,Wishlistbook: BookListing ) -> str:
-  self.books.append(book)
-  print(f'Book: {book.getBookID()} added to the wishlist.')
+    # Adding a book from the BookListing to the Wishlist
+    def addToWishlist(self, Wishlistbook: 'BookListing') -> str:
+        self.books.append(Wishlistbook)
+        print(f'Book: {Wishlistbook.getBookID()} added to the wishlist.')
 
-# Removing a book from the Wishlist if the book is in the wishlist
-  def removeBook(self, bookID: str) -> str:
-    for book in self.books
-      if book.getBookID() == bookID:
-        self.books.remove(book)
-        print(f' Book: {bookID} was removed from the wishlist.')
-      else:
-        print(f' Book with ID {bookID} was not found in the wishlist.')
+    # Removing a book from the Wishlist if the book is in the wishlist
+    def removeBook(self, bookID: int) -> str:
+        for book in self.books:
+            if book.getBookID() == bookID:
+                self.books.remove(book)
+                print(f'Book: {bookID} was removed from the wishlist.')
+                return
+        print(f'Book with ID {bookID} was not found in the wishlist.')
 

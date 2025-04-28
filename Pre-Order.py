53f0a1d75a41ca6d8da2c8a9710d57d18ba4e97b
ElisaMujica <elisa.m.mujica@gmail.com>
2025-04-28 15:34:21 -0500

    Update Pre-Order.py: Fix typos

--- Pre-Order.py
+++ Pre-Order.py
@@ -1,25 +1,25 @@
 class PreOrder:
 # Creating a new PreOrder object when called
-  def _init_(self):
+  def __init__(self):
     self.bookID = 0
     self.customerName = ""
     self.customerEmail = ""
 
 # Getting the book's ID number
-  def getBookId() -> int:
+  def getBookId(self) -> int:
     return self.bookID
 
 # Getting the name of the customer
-  def getCustomerName() -> string:
+  def getCustomerName(self) -> str:
     return self.customerName
 
 # Getting the customers email
-  def getCustomerEmail() -> string:
+  def getCustomerEmail(self) -> str:
     return self.customerEmail
 
 # Customer Inputting their pre-order details
   def addPreorder(self) -> str:
     self.bookID = int(input("Enter book ID to Pre-Order: "))
     self.customerName = input("Enter customer name: ")
-    self.customerEmail = inpuy ("Enter customer email: ")
+    self.customerEmail = input ("Enter customer email: ")
     return "Pre-Order placed succeeefully"

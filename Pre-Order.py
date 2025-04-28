18c79a616534d7781eab6cace7ea9418bfb7b57a
ElisaMujica <elisa.m.mujica@gmail.com>
2025-04-28 15:35:23 -0500

    Update Pre-Order.py: Fix structure

--- Pre-Order.py
+++ Pre-Order.py
@@ -1,25 +1,28 @@
 class PreOrder:
-# Creating a new PreOrder object when called
-  def __init__(self):
-    self.bookID = 0
-    self.customerName = ""
-    self.customerEmail = ""
+    # Creating a new PreOrder object when called
+    def __init__(self):
+        self.bookID = 0
+        self.customerName = ""
+        self.customerEmail = ""
 
-# Getting the book's ID number
-  def getBookId(self) -> int:
-    return self.bookID
+    # Getting the book's ID number
+    def getBookId(self) -> int:
+        return self.bookID
 
-# Getting the name of the customer
-  def getCustomerName(self) -> str:
-    return self.customerName
+    # Getting the name of the customer
+    def getCustomerName(self) -> str:
+        return self.customerName
 
-# Getting the customers email
-  def getCustomerEmail(self) -> str:
-    return self.customerEmail
+    # Getting the customer's email
+    def getCustomerEmail(self) -> str:
+        return self.customerEmail
 
-# Customer Inputting their pre-order details
-  def addPreorder(self) -> str:
-    self.bookID = int(input("Enter book ID to Pre-Order: "))
-    self.customerName = input("Enter customer name: ")
-    self.customerEmail = input ("Enter customer email: ")
-    return "Pre-Order placed succeeefully"
+    # Customer inputting their pre-order details
+    def addPreorder(self) -> str:
+        try:
+            self.bookID = int(input("Enter book ID to Pre-Order: "))
+        except ValueError:
+            return "Invalid Book ID. Pre-Order failed."
+        self.customerName = input("Enter customer name: ").strip()
+        self.customerEmail = input("Enter customer email: ").strip()
+        return "Pre-Order placed successfully."

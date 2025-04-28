d34790efb138220d850d0309d2dd1ad44c14c26b
ElisaMujica <elisa.m.mujica@gmail.com>
2025-04-28 14:08:40 -0500

    Update Outline1.py: Added BooklistingInterface
    Should come after Booklisting class. Not tested yet

--- Outline1.py
+++ Outline1.py
@@ -26,6 +26,18 @@ class BookListing:
     #Get current price(added for get price)
     def getPrice(self) -> float:
         return self.bookPrice
+
+class BookListingInterface:
+
+    @staticmethod
+    def displayBookListing(book: 'BookListing'):
+        # Display the details of a BookListing object
+        print("\n--- Book Listing ---")
+        print(f"The book ID: {book.getBookID()}")
+        print(f"The book was published by: {book.getBookPublishing()}")
+        print(f"The date the book was published: {book.getPublishingDate()}")
+        print(f"The price of the book: ${book.getPrice():.2f}")
+
         
 class LoginInterface: # create class to handle user login 
     def __init__(self): # initialize function to call other classes to retreive info

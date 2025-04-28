a95ddee337bb12b453ca8f9b988e91c2c5d33449
ElisaMujica <elisa.m.mujica@gmail.com>
2025-04-28 14:06:00 -0500

    Update Outline1: Added Booklisting Class
    Not tested yet. Class should go before LoginInterface before all else.

--- Outline1.py
+++ Outline1.py
@@ -1,3 +1,32 @@
+
+class BookListing:
+    #Constructor to initialize a book listing (added to created book and set values)
+    def __init__(self, bookID: int, bookPublishing: str, bookPublishedDate: str, bookPrice: float):
+        self.bookID = bookID
+        self.bookPublishing = bookPublishing
+        self.bookPublishedDate = bookPublishedDate
+        self.bookPrice = bookPrice
+
+    #Getter for bookID
+    def getBookID(self) -> int:
+        return self.bookID
+
+    #Getter for bookPublishing
+    def getBookPublishing(self) -> str:
+        return self.bookPublishing
+
+    #Getter for bookPublishedDate
+    def getPublishingDate(self) -> str:
+        return self.bookPublishedDate
+
+    #Setter for bookPrice
+    def setPrice(self, newPrice: float):
+        self.bookPrice = newPrice
+
+    #Get current price(added for get price)
+    def getPrice(self) -> float:
+        return self.bookPrice
+        
 class LoginInterface: # create class to handle user login 
     def __init__(self): # initialize function to call other classes to retreive info
         self.UserAccountManager = UserAccountManager()
